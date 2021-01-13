ea62bc07a64d68043d5e90bef8499c3f5c01b26f
monroekimmel <monroekimmel@gmail.com>
2021-01-13 18:34:02 -0500

    New updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
 
 const routes: Routes = [
-  {path:'', redirectTo: 'actor/list', pathMatch: 'full' },
-  {path:'actor/list', component: ActorListComponent},
-  {path:'**', component: ActorListComponent}
+{path:, redirectTo: '/hoome', pathMatch: 'full'},
+{path: '/movie-list', component: MovieListComponent},
+{path: '/movie-create', component: MovieCreateComponent},
+{path: '/actor-list', component: ActorListComponent},
+{path: '**', component: noComponentDefinedComponent}
 ];
 
 @NgModule({
